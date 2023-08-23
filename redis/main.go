92aac568241b6f884e763f9a203faa06531327ba
StarBus <xzc2860072080@gmail.com>
2023-08-23 11:30:43 +0800

    feat: change redis's example code

--- redis/main.go
+++ redis/main.go
@@ -1,19 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
+)
 
 func main() {
-	//rdb := redis.NewClient(&redis.Options{
-	//	Addr:     "127.0.0.1:6379",
-	//	Password: "", // no password set
-	//	DB:       0,  // use default DB
-	//})
-	//
-	//name := rdb.Get(context.Background(), "name").Val()
-	//fmt.Printf("name=%s\n", name)
-	//fmt.Println(65536 >> 16)
-	//fmt.Println(131072 >> 16)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-	fmt.Println(1 ^ (1 << 16))
-	fmt.Println(1 ^ (2 << 16))
+	name := rdb.Get(context.Background(), "name").Val()
+	log.Println(name)
 }
