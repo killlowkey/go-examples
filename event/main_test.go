05b86ae092a97dce4c6cafb5bddd6728998258b2
StarBus <xzc2860072080@gmail.com>
2023-12-21 11:05:57 +0800

    feat: add event

---
+++ event/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
+	"testing"
+	"time"
+)
+
+func publishMessages(publisher message.Publisher) {
+	for {
+		msg := message.NewMessage(watermill.NewUUID(), []byte("Hello, world!"))
+
+		if err := publisher.Publish("example.topic", msg); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		fmt.Printf("received message: %s, payload: %s\n", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
+}
+
+func Test_GoChannel(t *testing.T) {
+	pubSub := gochannel.NewGoChannel(
+		gochannel.Config{},
+		watermill.NewStdLogger(false, false),
+	)
+
+	messages, err := pubSub.Subscribe(context.Background(), "example.topic")
+	if err != nil {
+		panic(err)
+	}
+
+	go process(messages)
+	publishMessages(pubSub)
+}
